9f40541505f460d6c90a27b826d76c94da9cc0fa
Ikey Benzaken <ikey.benz@gmail.com>
2019-10-02 13:25:36 -0700

    started adding Game and Player classes

---
+++ backend/src/controllers/Game.js
@@ -0,0 +1,16 @@
+class ModiGame {
+  constructor(players) {
+
+  }
+}
+
+class Player {
+  constructor(connectionId) {
+    this.connectionId = connectionId;
+    this.id = this.generateNewUserId();
+  }
+  generateNewUserId() {
+
+  }
+}
+
