d73cc761b946fb6994d40ae6395941b7c981f4fc
IkeyBenz <ikey.benz@gmail.com>
2019-10-17 10:43:12 -0700

    Made create lobby page

---
+++ frontend/src/pages/CreateLobbyPage.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { create } from '../api/lobby-interface';
+
+const CreateLobbyPage = () => {
+
+  const [lobbyName, setLobbyName] = useState('');
+
+  const createLobby = async () => {
+    const res = await create(lobbyName);
+    if (res.error) {
+      return alert(`Error: ${res.error}`);
+    }
+    window.location.replace(`/lobbies/${lobbyName}`);
+  }
+
+  return (
+    <div className="container-fluid">
+      <div className="card">
+        <div className="card-header">
+          <h3 className="card-title">Create Lobby:</h3>
+        </div>
+        <div className="card-body">
+          <div className="input-group">
+            <input className="form-control"
+              type="text"
+              value={lobbyName}
+              placeholder="Enter lobby name..."
+              onChange={(e) => setLobbyName(e.target.value)}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary" onClick={createLobby} disabled={lobbyName === ''}>Create &rarr;</button>
+            </div>
+          </div>
+          <div className="card-footer">
+            <a className="btn btn-danger" href="/">&larr; Back</a>
+          </div>
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default CreateLobbyPage;
