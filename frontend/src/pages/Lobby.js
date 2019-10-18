ce7eaa1854501488201e62e50d626709681b440a
IkeyBenz <ikey.benz@gmail.com>
2019-10-18 01:11:27 -0700

    [polish] added logic for lobby admins

--- frontend/src/pages/Lobby.js
+++ frontend/src/pages/Lobby.js
@@ -1,43 +1,50 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCrown } from '@fortawesome/free-solid-svg-icons';
 import io from 'socket.io-client';
-import { get } from '../api/lobby-interface';
 
 let socket;
-let lobbyName;
 function LobbyPage({ match }) {
 
-  const [users, setUsers] = useState([]);
+  const [lobbyInfo, setLobbyInfo] = useState({});
+  const { lobbyName, players, creator } = lobbyInfo;
   const { id: lobbyId } = match.params;
+
+  if (!sessionStorage.getItem(`modi-${lobbyId}-username`)) {
+    sessionStorage.setItem(`modi-${lobbyId}-username`, prompt('Enter your username:'));
+  }
 
   !socket && (socket = io(`/lobbies/${lobbyId}`));
   socket.on('connect', () => {
-    socket.emit('join-attempt', localStorage.getItem('modi-username'));
-    socket.on('updated-lobby', setUsers);
+    socket.emit('join-attempt', sessionStorage.getItem(`modi-${lobbyId}-username`));
+    socket.on('updated-lobby', setLobbyInfo);
   });
 
-  !lobbyName && ((async () => {
-    const { info, error } = await get(lobbyId);
-    if (error)
-      return alert(error);
+  const isAdmin = socket.id === creator;
+  const createGame = () => {
 
-    lobbyName = info.name;
-  })());
+  }
 
   return (
     <div className="container-fluid">
-      <div className="card">
+      <div className="card mt-3">
         <div className="card-header">
-          <h3 className="card-title">Lobby #{lobbyName}</h3>
+          {lobbyName && <h3 className="card-title">{lobbyName}</h3>}
         </div>
         <div className="card-body">
           <ul className="list-group">
-            {users.map((username, ind) => (
-              <li key={ind} className="list-group-item">{username}</li>
+            {players && Object.keys(players).map((connectionId) => (
+              <li key={connectionId} className="list-group-item">
+                {connectionId === creator && <FontAwesomeIcon icon={faCrown} />}
+                {" "}
+                {players[connectionId]}
+              </li>
             ))}
           </ul>
         </div>
-        <div className="card-footer">
+        <div className="card-footer d-flex justify-content-between">
           <a className="btn btn-danger" href="/lobbies">&larr; Back</a>
+          {isAdmin && <button className="btn btn-primary" onClick={createGame}>Start &rarr;</button>}
         </div>
       </div>
     </div>
